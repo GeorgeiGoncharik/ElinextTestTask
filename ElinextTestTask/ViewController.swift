0446273d406762de4ace90bf728d803c95d06c9a
Georgei Goncharik <georgii.goncharik@gmail.com>
2021-07-19 13:50:57 +0300

    Initial Commit

---
+++ ElinextTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElinextTestTask
+//
+//  Created by Mac on 19.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
